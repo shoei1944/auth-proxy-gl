21185dbbb4a6c2bd07d9ffbcd6723784e85b8df7
SwellLobster338 <77489285+SwellLobster338@users.noreply.github.com>
2025-03-13 02:14:55 +0300

    Update response.rs

--- src/launcher/types/response.rs
+++ src/launcher/types/response.rs
@@ -1,15 +1,21 @@
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(bound = "T: Serialize + DeserializeOwned")]
-pub struct Response<T: Serialize + DeserializeOwned> {
+
+[derive(Serialize, Deserialize, Debug)]
+
+[serde(bound = "T: Serialize + DeserializeOwned")]
+
+pub struct Response {
     #[serde(rename = "requestUUID")]
     pub id: Uuid,
 
-    #[serde(flatten)]
-    pub body: T,
+
+#[serde(flatten)]
+pub body: T,
+
 }
+
 
 pub mod any {
     use crate::launcher::types::response::{
@@ -18,179 +24,185 @@ pub mod any {
     };
     use serde::{Deserialize, Serialize};
 
-    pub type Any = Response<Kind>;
 
-    #[derive(Serialize, Deserialize, Debug)]
-    #[serde(tag = "type")]
-    pub enum Kind {
-        #[serde(rename = "restore")]
-        RestoreToken(restore_token::RestoreToken),
+pub type Any = Response<Kind>;
 
-        #[serde(rename = "checkServer")]
-        CheckServer(check_server::CheckServer),
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "type")]
+pub enum Kind {
+    #[serde(rename = "restore")]
+    RestoreToken(restore_token::RestoreToken),
 
-        #[serde(rename = "profileByUUID")]
-        GetProfileByUuid(get_profile_by_uuid::GetProfileByUuid),
+    #[serde(rename = "checkServer")]
+    CheckServer(check_server::CheckServer),
 
-        #[serde(rename = "profileByUsername")]
-        GetProfileByUsername(get_profile_by_username::GetProfileByUsername),
+    #[serde(rename = "profileByUUID")]
+    GetProfileByUuid(get_profile_by_uuid::GetProfileByUuid),
 
-        #[serde(rename = "batchProfileByUsername")]
-        BatchProfilesByUsernames(batch_profiles_by_usernames::BatchProfilesByUsernames),
+    #[serde(rename = "profileByUsername")]
+    GetProfileByUsername(get_profile_by_username::GetProfileByUsername),
 
-        #[serde(rename = "error")]
-        Error(error::Error),
-    }
+    #[serde(rename = "batchProfileByUsername")]
+    BatchProfilesByUsernames(batch_profiles_by_usernames::BatchProfilesByUsernames),
+
+    #[serde(rename = "error")]
+    Error(error::Error),
 }
+
+}
+
 
 pub mod error {
     use serde::{Deserialize, Serialize};
 
-    #[derive(Serialize, Deserialize, Debug)]
-    pub struct Error {
-        #[serde(rename = "error")]
-        pub kind: Kind,
-    }
 
-    #[derive(Serialize, Deserialize, Debug)]
-    pub enum Kind {
-        #[serde(rename = "User not found")]
-        UserNotFound,
-
-        #[serde(rename = "User not verified")]
-        UserNotVerified,
-
-        #[serde(rename = "Permissions denied")]
-        PermissionsDenied,
-
-        #[serde(untagged)]
-        Other(String),
-    }
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Error {
+    #[serde(rename = "error")]
+    pub kind: Kind,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Kind {
+    #[serde(rename = "User not found")]
+    UserNotFound,
+
+    #[serde(rename = "User not verified")]
+    UserNotVerified,
+
+    #[serde(rename = "Permissions denied")]
+    PermissionsDenied,
+
+    #[serde(untagged)]
+    Other(String),
+}
+
+}
+
 
 pub mod restore_token {
     use serde::{Deserialize, Serialize};
 
-    #[derive(Serialize, Deserialize, Debug)]
-    pub struct RestoreToken {
-        #[serde(rename = "invalidTokens")]
-        pub invalid_tokens: Vec<String>,
-    }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RestoreToken {
+    #[serde(rename = "invalidTokens")]
+    pub invalid_tokens: Vec<String>,
 }
+
+}
+
 
 pub mod check_server {
     use serde::{Deserialize, Serialize};
     use uuid::Uuid;
 
-    #[derive(Serialize, Deserialize, Debug)]
-    pub struct CheckServer {
-        pub uuid: Uuid,
 
-        #[serde(rename = "playerProfile")]
-        pub profile: Profile,
-    }
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CheckServer {
+    pub uuid: Uuid,
 
-    #[derive(Serialize, Deserialize, Debug)]
-    pub struct Profile {
-        pub uuid: Uuid,
-        pub username: String,
-    }
+    #[serde(rename = "playerProfile")]
+    pub profile: Profile,
 }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Profile {
+    pub uuid: Uuid,
+    pub username: String,
+}
+
+}
+
 
 pub mod get_profile_by_uuid {
     use crate::launcher::types::response::base;
     use serde::{Deserialize, Serialize};
 
-    #[derive(Serialize, Deserialize, Debug)]
-    pub struct GetProfileByUuid {
-        #[serde(rename = "playerProfile")]
-        pub player_profile: base::profile::Profile,
-    }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct GetProfileByUuid {
+    #[serde(rename = "playerProfile")]
+    pub player_profile: base::profile::Profile,
 }
+
+}
+
 
 pub mod get_profile_by_username {
     use crate::launcher::types::response::base;
     use serde::{Deserialize, Serialize};
 
-    #[derive(Serialize, Deserialize, Debug)]
-    pub struct GetProfileByUsername {
-        #[serde(rename = "playerProfile")]
-        pub player_profile: base::profile::Profile,
-    }
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct GetProfileByUsername {
+    #[serde(rename = "playerProfile")]
+    pub player_profile: base::profile::Profile,
 }
+
+}
+
 
 pub mod base {
     pub mod profile {
         use serde::{Deserialize, Serialize};
         use uuid::Uuid;
 
-        #[derive(Serialize, Deserialize, Debug)]
-        pub struct Profile {
-            pub uuid: Uuid,
-            pub username: String,
-            pub assets: Assets,
-        }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Profile {
+        pub uuid: Uuid,
+        pub username: String,
+        pub assets: Assets,
+    }
+
+    #[derive(Serialize, Deserialize, Debug)]
+    pub struct Assets {
+        #[serde(rename = "SKIN", skip_serializing_if = "Option::is_none")]
+        pub skin: Option<skin::Skin>,
+
+        #[serde(rename = "CAPE", skip_serializing_if = "Option::is_none")]
+        pub cape: Option<cape::Cape>,
+    }
+
+    pub mod skin {
+        use serde::{Deserialize, Serialize};
 
         #[derive(Serialize, Deserialize, Debug)]
-        pub struct Assets {
-            #[serde(rename = "SKIN", skip_serializing_if = "Option::is_none")]
-            pub skin: Option<skin::Skin>,
-
-            #[serde(rename = "CAPE", skip_serializing_if = "Option::is_none")]
-            pub cape: Option<cape::Cape>,
-        }
-
-        pub mod skin {
-            use serde::{Deserialize, Serialize};
-
-            #[derive(Serialize, Deserialize, Debug)]
-            pub struct Skin {
-                pub url: String,
-                pub digest: String,
-
-                #[serde(skip_serializing_if = "Option::is_none")]
-                pub metadata: Option<metadata::Metadata>,
+        pub struct SkinSerialize, Deserialize, Debug)]
+            pub struct Metadata {
+                #[serde(default)]
+                pub model: Model,
             }
 
-            pub mod metadata {
-                use serde::{Deserialize, Serialize};
+            #[derive(Serialize, Deserialize, Default, Debug, Eq, PartialEq)]
+            pub enum Model {
+                #[serde(rename = "classic")]
+                Classic,
 
-                #[derive(Serialize, Deserialize, Debug)]
-                pub struct Metadata {
-                    #[serde(default)]
-                    pub model: Model,
-                }
-
-                #[derive(Serialize, Deserialize, Default, Debug, Eq, PartialEq)]
-                pub enum Model {
-                    #[serde(rename = "slim")]
-                    Slim,
-
-                    #[serde(rename = "default")]
-                    #[default]
-                    Default,
-                }
-            }
-        }
-
-        pub mod cape {
-            use serde::{Deserialize, Serialize};
-
-            #[derive(Serialize, Deserialize, Debug)]
-            pub struct Cape {
-                pub url: String,
+                #[serde(rename = "default")]
+                #[default]
+                Default,
             }
         }
     }
+
+    pub mod cape {
+        use serde::{Deserialize, Serialize};
+
+        #[derive(Serialize, Deserialize, Debug)]
+        pub struct Cape {
+            pub url: String,
+        }
+    }
 }
+
+}
+
 
 pub mod batch_profiles_by_usernames {
     use crate::launcher::types::response::base::profile;
     use serde::{Deserialize, Serialize};
 
-    #[derive(Serialize, Deserialize, Debug)]
-    pub struct BatchProfilesByUsernames {
-        #[serde(rename = "playerProfiles")]
-        pub player_profiles: Vec<profile::Profile>,
-    }
-}
+
+#[derive(Serialize, Deserialize, Debug)]
